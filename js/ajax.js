2838833ab944a9e38da532f10d8234a2dd2b8c23
user <user@mail.com>
2022-09-06 12:24:28 -0500

    Actualizacion, mostrar data obtenida

--- js/ajax.js
+++ js/ajax.js
@@ -2,37 +2,55 @@
 
     $.ajax({
         type: 'GET',
-        url:'https://restcountries.com/v3.1/name/mex', 
+        url: 'https://restcountries.com/v3.1/all',
         //'https://5d6d-2806-106e-15-70f9-a4c3-f01-bc33-c341.ngrok.io/MWDispatcher-2.0/toa/solicitarAjusteVelocidad',
         dataType: 'json'
     }).done((data) => {
-        console.log('Hecho Correcto');
-        console.log(':::'+JSON.stringify(data));
+        console.log('data:::' + data.length);
+        /* console.log('Hecho Correcto');
+        console.log(':::'+JSON.stringify(data)); */
         //let respuesta=JSON.stringify(data);        
-        let respuesta=data;        
+        /* let respuesta=data;        
         console.log('data:::'+respuesta.length);
-
         let pais= respuesta[0];
         console.log('respuesta:::'+respuesta);
-        console.log('tipo:::'+ typeof respuesta);
+        console.log('tipo:::'+ typeof respuesta); */
         //$("#picFoto").attr('src',data.picture);
-        $("#txtNombre").val(respuesta[0].cca2);
+        /* $("#txtNombre").val(respuesta[0].cca2);
          $("#txtDireccion").val(pais.name.common);
-        $("#txtTelefono").val(data[0].altSpellings[2]);
-       /*  $("#txtGenero").val(respuesta.ipResponse); */
+        $("#txtTelefono").val(data[0].altSpellings[2]); */
+        /*  $("#txtGenero").val(respuesta.ipResponse); */
+        $.each(data, function (key, registro) {
+            $("#Select").append('<option value='+(parseInt(key)+1)+'>' + registro.name.common + '</option>');
+        });
+
+        data.forEach((registro,key)=>{
+            let content = "";
+            content += '<tr>';
+            content += '    <td>'+(parseInt(key)+1)+'</td>';
+            content += '    <td>'+registro.name.common+'</td>';
+            content += '    <td class="text-center"><a href="" class="btn btn-danger"><i class="fa fa-trash-o"></i></a></td>';
+            content += '    <td class="text-center"><a href="" class="btn btn-primary"><i class="fa fa-edit"></i></a></td>';
+            content += '</tr>';
+
+            $('#tblRegistros').append(content);
+        })
+
     }
-    ).fail(()=>{
+    ).fail(() => {
         console.log("Fallo");
     }
-    ).always(()=>{
+    ).always(() => {
         console.log('Completo');
     })
 
+    const llamarFuncion=()=>{
+        alert('Presionado llamarFuncion');
+    }
 
+    $("#btnInsert").on("click", llamarFuncion);
 
-
-
-
-
-
+    $("#Select").on("click", ()=>{
+        alert('--');
+    });
 })();
