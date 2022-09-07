4c83853e681bb3244688aea3d32bf423338e61e3
user <user@mail.com>
2022-09-06 23:03:39 -0500

    Combos Dependientes

--- js/ajax.js
+++ js/ajax.js
@@ -21,10 +21,28 @@
         $("#txtTelefono").val(data[0].altSpellings[2]); */
         /*  $("#txtGenero").val(respuesta.ipResponse); */
         $.each(data, function (key, registro) {
-            $("#Select").append('<option value='+(parseInt(key)+1)+'>' + registro.name.common + '</option>');
+            $("#Select").append('<option value=' + (parseInt(key) + 1) + '>' + registro.name.common + '</option>');
         });
 
-        data.forEach((registro,key)=>{
+        let datos = data;
+        for (let i = 0; i < datos.length; i++) {
+            let dato = datos[i];
+            let latlng = '';
+            for (let j = 0; j < dato.latlng.length; j++) {
+                latlng += '<span class="label label-primary">' + dato.latlng[j] + '</span> ';
+            }
+            let content = "";
+            content += '<tr>';
+            content += '    <td>' + (i + 1) + '</td>';
+            content += '    <td>' + dato.name.common + '</td>';
+            content += '    <td>' + latlng + '</td>';
+            content += '    <td class="text-center"><a href="" class="btn btn-danger"><i class="fa fa-trash-o"></i></a></td>';
+            content += '    <td class="text-center"><a href="" class="btn btn-primary"><i class="fa fa-edit"></i></a></td>';
+            content += '</tr>';
+
+            $('#tblRegistros').append(content);
+        }
+        /* data.forEach((registro,key)=>{
             let content = "";
             content += '<tr>';
             content += '    <td>'+(parseInt(key)+1)+'</td>';
@@ -34,7 +52,7 @@
             content += '</tr>';
 
             $('#tblRegistros').append(content);
-        })
+        }) */
 
     }
     ).fail(() => {
@@ -44,13 +62,27 @@
         console.log('Completo');
     })
 
-    const llamarFuncion=()=>{
+    const llamarFuncion = () => {
         alert('Presionado llamarFuncion');
     }
 
     $("#btnInsert").on("click", llamarFuncion);
 
-    $("#Select").on("click", ()=>{
-        alert('--');
+    $("#Select").on("change", () => {
+        let seleccion = $("#Select option:selected").text();
+        console.log("Has seleccionado: - " + $("#Select option:selected").val() + '--' + seleccion);
+        $.ajax({
+            type: 'GET',
+            url: 'https://restcountries.com/v3.1/name/'+ seleccion,
+            //'https://5d6d-2806-106e-15-70f9-a4c3-f01-bc33-c341.ngrok.io/MWDispatcher-2.0/toa/solicitarAjusteVelocidad',
+            dataType: 'json'
+        }).done((data) => {
+            console.log('data:::'+data);
+        }).fail(() => {
+            console.log("Fallo");
+        }
+        ).always(() => {
+            console.log('Completo');
+        });
     });
 })();
